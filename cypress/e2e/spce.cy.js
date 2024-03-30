11e0c4a6c1c4bfe025cc45cf144b410b54f95bf3
MalikHassan26 <malikhassan2611@gmail.com>
2024-03-30 23:02:22 +0500

    Convert line endings

---
+++ cypress/e2e/spce.cy.js
@@ -0,0 +1,11 @@
+it('Testing The Login page' , () => {
+    cy.visit('/signin' ,{timeout : 6000 })
+
+    cy.get(':nth-child(1) > .w-full').type('tester@yopmail.com') 
+    cy.get('.relative > .w-full').type('qwerta1')
+    cy.get('.bg-custom-dark').click('')
+    cy.get('.mb-auto > .flex-col > :nth-child(3) > .flex').click('')
+    cy.get(':nth-child(1) > a > .group > .flex-col').click('')
+    
+    cy.get('.px-10').click('')
+});
